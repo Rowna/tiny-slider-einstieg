4c25a6a42610803c0c01a91e3ade79befb7ab068
Rowan kh <rowan.kharnoub@adesso.de>
2022-01-17 13:50:50 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// const { tns } = require("tiny-slider");
+
+var slider = tns({
+    "container": "#my-slider",
+    "items": 1,
+    "controls": false,
+    "mouseDrag": true,
+    "slideBy": 1,
+    "navPosition": "bottom",
+    "swipeAngle": false,
+    "speed": 400,
+    "preventScrollOnTouch": 'auto'
+});
